1a3dd701d9919163ddc3005abb9eedc199cf9996
gitpremsagar <psagar172@gmail.com>
2022-02-12 20:06:49 +0530

    rendering asideleft nav items by looping through categories Ojject

---
+++ components/CategoriesSection/categoriesObject.js
@@ -0,0 +1,10 @@
+export const categoriesObject = {
+  "Design & Graphics": {
+    "Logo and Brand": ["Logo", "Brand Style"],
+    Art: ["Illustration", "NFT Art", "Others"],
+  },
+  "Web Devlopment": {
+    Frontend: ["React", "Next", "Veu", "Other"],
+    Backend: ["Node", "Express", "GraphQL"],
+  },
+};
--- components/Theme/AsideLeft/AsideLeft.js
+++ components/Theme/AsideLeft/AsideLeft.js
@@ -1,18 +1,27 @@
+import { categoriesObject } from "../../CategoriesSection/categoriesObject";
+
 export default function AsideLeft() {
   return (
     <aside className="h-screen overflow-y-scroll bg-gray-800 text-white w-full sticky top-0">
-      <ul className="">
-        {[
-          "Home",
-          "Design & Graphics",
-          "Web Devlopment",
-          "App Development",
-          "Writing",
-          "Digital Marketing",
-          "Video & Audio",
-          "Business",
-          "Trending",
-        ].map((category, key) => {
+      <ul>
+        <li className=" px-2 py-4 hover:bg-gray-900">
+          Home
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 float-left mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+            />
+          </svg>
+        </li>
+        {Object.keys(categoriesObject).map((category, key) => {
           return (
             <li key={key} className=" px-2 py-4 hover:bg-gray-900">
               {/* Category icons */}
@@ -47,15 +56,29 @@ export default function AsideLeft() {
                 />
               </svg>
               {/* List of items within the category */}
-              {category !== "Home" ? (
-                <ul className="ml-8 h-0 overflow-hidden">
-                  {["item 1", "item 2", "item 3", "item 4"].map(
-                    (categoryItem, key) => {
-                      return <li key={key}>{categoryItem}</li>;
-                    }
-                  )}
-                </ul>
-              ) : null}
+
+              <ul className="ml-10 mt-5">
+                {Object.keys(categoriesObject[category]).map(
+                  (subCategory, key) => {
+                    return (
+                      <li key={key}>
+                        <span className="underline underline-offset-4">
+                          {subCategory}
+                        </span>
+                        <ul className="ml-5 mt-4 mb-4">
+                          {Object.values(
+                            categoriesObject[category][subCategory].map(
+                              (serivceList, key) => {
+                                return <li key={key}>* {serivceList}</li>;
+                              }
+                            )
+                          )}
+                        </ul>
+                      </li>
+                    );
+                  }
+                )}
+              </ul>
             </li>
           );
         })}
