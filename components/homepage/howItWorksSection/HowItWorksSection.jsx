20fbce8e6c72dc3e85374d1cd9b51c8cb75af6ea
gitpremsagar <psagar172@gmail.com>
2023-11-06 23:28:56 +0530

    style - creates CTA buttons on how it works section on homepage.

--- components/homepage/howItWorksSection/HowItWorksSection.jsx
+++ components/homepage/howItWorksSection/HowItWorksSection.jsx
@@ -3,6 +3,7 @@ import Section from "../../UI/Section";
 import H3 from "../../UI/H3";
 import H4 from "../../UI/H4";
 import H6 from "./../../UI/H6";
+import ButtonPrimary from "../../UI/ButtonPrimary";
 
 export default function HowItWorksSection() {
   return (
@@ -12,15 +13,23 @@ export default function HowItWorksSection() {
       <H4>You can hire someone in 2 ways:</H4>
       <div className="grid grid-cols-2 gap-2">
         <div className="bg-blue-200 p-6 rounded">
-          <H6>First Way</H6>
-          Either post a job which will create a "project" on our platform on
-          which freelancers can bid. You get to decide which bid to accept and
-          handover the job.
+          <div className="mb-5">
+            <H6>First Way</H6>
+            Either post a job which will create a "project" on our platform on
+            which freelancers can bid. You get to decide which bid to accept and
+            handover the job.
+          </div>
+          <ButtonPrimary className={`float-right`}>Post s job</ButtonPrimary>
         </div>
         <div className="bg-green-200 p-6 rounded">
-          <H6>Second Way</H6>
-          Or order from many predefined services called as "proposals" which are
-          provided by freelancers listed on our platform.
+          <div className="mb-5">
+            <H6>Second Way</H6>
+            Or order from many predefined services called as "proposals" which
+            are provided by freelancers listed on our platform.
+          </div>
+          <ButtonPrimary className={`float-right`}>
+            Browse proposals
+          </ButtonPrimary>
         </div>
       </div>
     </Section>
